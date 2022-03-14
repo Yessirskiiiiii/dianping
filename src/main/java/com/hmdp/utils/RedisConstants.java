3413aad21daedbdbd409bed702973f8cd07ba06a
BaoShaWang <674134561@qq.com>
2022-03-14 21:47:17 +0800

    解决缓存穿透问题，商铺不存在时将null值写入Redis

--- src/main/java/com/hmdp/utils/RedisConstants.java
+++ src/main/java/com/hmdp/utils/RedisConstants.java
@@ -7,4 +7,6 @@ public class RedisConstants {
     public static final Long LOGIN_USER_TTL = 30L;
     public static final String CACHE_SHOP_KEY = "cache:shop:";
     public static final Long CACHE_SHOP_TTL = 30L;
+
+    public static final Long CACHE_NULL_TTL = 2L;
 }
