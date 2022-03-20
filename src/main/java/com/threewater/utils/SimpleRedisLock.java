8ea552547678b8e060f1af9b64aa22d0db462109
BaoShaWang <674134561@qq.com>
2022-03-20 20:10:26 +0800

    继续优化Redis分布式锁：调用Lua脚本保证 1.比较标识 与 2.判断标识是否相同 这两个操作的原子性，相当于MySQL中事务的作用

--- src/main/java/com/threewater/utils/SimpleRedisLock.java
+++ src/main/java/com/threewater/utils/SimpleRedisLock.java
@@ -1,8 +1,11 @@
 package com.threewater.utils;
 
 import cn.hutool.core.lang.UUID;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -22,6 +25,13 @@ public class SimpleRedisLock implements ILock {
 
     private static final String KEY_PREFIX = "lock:";
     private static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
+    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+
+    static {
+        UNLOCK_SCRIPT = new DefaultRedisScript<>();
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unLock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
 
     @Override
     public boolean tryLock(long timeoutSec) {
@@ -34,6 +44,12 @@ public class SimpleRedisLock implements ILock {
 
     @Override
     public void unLock() {
+        // 调用Lua脚本 相当于MySQL中的事务操作
+        stringRedisTemplate.execute(UNLOCK_SCRIPT, Collections.singletonList(KEY_PREFIX + name), ID_PREFIX + Thread.currentThread().getId());
+    }
+
+    /*@Override
+    public void unLock() {
         // 获取线程标识
         String threadId = ID_PREFIX + Thread.currentThread().getId();
         // 获取锁中的标识
@@ -43,5 +59,5 @@ public class SimpleRedisLock implements ILock {
             // 释放锁
             stringRedisTemplate.delete(KEY_PREFIX + name);
         }
-    }
+    }*/
 }
