88fc7d8a45f83a257ae561ae508c4b06fe68cb30
BaoShaWang <674134561@qq.com>
2022-03-25 11:34:41 +0800

    实现关注用户以及取关用户的功能

--- src/main/java/com/threewater/service/IFollowService.java
+++ src/main/java/com/threewater/service/IFollowService.java
@@ -1,5 +1,6 @@
 package com.threewater.service;
 
+import com.threewater.dto.Result;
 import com.threewater.entity.Follow;
 import com.baomidou.mybatisplus.extension.service.IService;
 
@@ -13,4 +14,7 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface IFollowService extends IService<Follow> {
 
+    Result follow(Long followUserId, Boolean isFollow);
+
+    Result isFollow(Long followUserId);
 }
