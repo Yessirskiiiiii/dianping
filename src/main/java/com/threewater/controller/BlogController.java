0b42d41700bf4ba9baa93a104f95da6c28753a39
BaoShaWang <674134561@qq.com>
2022-03-25 16:18:14 +0800

    基于Feed流的推模式实现关注推送功能 利用Redis中的sortedset实现滚动分页查询

--- src/main/java/com/threewater/controller/BlogController.java
+++ src/main/java/com/threewater/controller/BlogController.java
@@ -5,9 +5,7 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.threewater.dto.Result;
 import com.threewater.dto.UserDTO;
 import com.threewater.entity.Blog;
-import com.threewater.entity.User;
 import com.threewater.service.IBlogService;
-import com.threewater.service.IUserService;
 import com.threewater.utils.SystemConstants;
 import com.threewater.utils.UserHolder;
 import org.springframework.web.bind.annotation.*;
@@ -32,13 +30,7 @@ public class BlogController {
 
     @PostMapping
     public Result saveBlog(@RequestBody Blog blog) {
-        // 获取登录用户
-        UserDTO user = UserHolder.getUser();
-        blog.setUserId(user.getId());
-        // 保存探店博文
-        blogService.save(blog);
-        // 返回id
-        return Result.ok(blog.getId());
+        return blogService.saveBlog(blog);
     }
 
     @PutMapping("/like/{id}")
@@ -86,5 +78,10 @@ public class BlogController {
         // 获取当前页数据
         List<Blog> records = page.getRecords();
         return Result.ok(records);
+    }
+
+    @GetMapping("/of/follow")
+    public Result queryBlogOfFollow(@RequestParam("lastId") Long max, @RequestParam(value = "offset", defaultValue = "0") Integer offset) {
+        return blogService.queryBlogOfFollow(max, offset);
     }
 }
