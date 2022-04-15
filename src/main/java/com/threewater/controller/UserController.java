2360037dab394d0cd66b69351db3d5870e4ed5b0
BaoShaWang <674134561@qq.com>
2022-04-15 20:18:27 +0800

    利用Redis中的BitMap数据结构实现用户签到以及统计签到次数功能

--- src/main/java/com/threewater/controller/UserController.java
+++ src/main/java/com/threewater/controller/UserController.java
@@ -22,7 +22,7 @@ import javax.servlet.http.HttpSession;
  * 前端控制器
  * </p>
  *
- * @author 虎哥
+ * @author Yessirskiii
  * @since 2021-12-22
  */
 @Slf4j
@@ -100,5 +100,15 @@ public class UserController {
         UserDTO userDTO = BeanUtil.copyProperties(user, UserDTO.class);
         // 返回
         return Result.ok(userDTO);
+    }
+
+    @PostMapping("/sign")
+    public Result sign() {
+        return userService.sign();
+    }
+
+    @GetMapping("/sign/count")
+    public Result signCount() {
+        return userService.signCount();
     }
 }
--- src/main/java/com/threewater/utils/RedisConstants.java
+++ src/main/java/com/threewater/utils/RedisConstants.java
@@ -15,4 +15,6 @@ public class RedisConstants {
     public static final String SECKILL_STOCK_KEY = "seckill:stock:";
     public static final String BLOG_LIKED_KEY = "blog:liked:";
     public static final String FEED_KEY = "feed:";
+
+    public static final String USER_SIGN_KEY = "sign:";
 }
